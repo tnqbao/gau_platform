5c6e2b8f2b0cb490efb731e37f19536b5c15cef3
tnqbao <quocbao.job106204@gmail.com>
2025-06-16 01:49:58 +0700

    feat: add restart composes script

--- docker/scripts/restart.sh
+++ docker/scripts/restart.sh
@@ -9,7 +9,7 @@ docker-compose up -d
 
 cd ../base-application-zone || exit 1
 docker-compose down
-docker-compose up -d
+./restart_gau_account_service.sh
 
 cd service-zone || exit 1
 docker-compose down
