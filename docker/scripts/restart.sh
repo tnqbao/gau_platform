f680da376b911b02463a7eeb8ed50dfc45d857ba
Tran Nguyen Quoc Bao <tnqbao@homelab-bao.daudoo.com>
2025-06-19 00:20:05 +0700

    update: docker sycn permission file

--- docker/scripts/restart.sh
+++ docker/scripts/restart.sh
@@ -13,7 +13,7 @@ cd ../base-application-zone || exit 1
 docker-compose down
 ./restart_gau_account_service.sh
 
-cd service-zone || exit 1
+cd ../services-zone || exit 1
 docker-compose down
 docker-compose up -d
 
